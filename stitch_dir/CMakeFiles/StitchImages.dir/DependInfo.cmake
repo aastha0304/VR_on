dfdb26c4234b21e7fa2acf93eb8b654cfa964da4
aastha0304 <aastha.gsrai@gmail.com>
2017-01-16 12:02:05 +0530

    stitch_dir - a 360 image stitcher, webvr-boilerplate - view equirectangular images (or any high res image) in 360

---
+++ stitch_dir/CMakeFiles/StitchImages.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/aastha/codes/workspace/experimentWithVR/stitch_dir/stitching_detailed.cpp" "/Users/aastha/codes/workspace/experimentWithVR/stitch_dir/CMakeFiles/StitchImages.dir/stitching_detailed.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/Users/aastha/homebrew/include/opencv"
+  "/Users/aastha/homebrew/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ stitch_dir/CMakeFiles/StitchImages.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/StitchImages.dir/stitching_detailed.cpp.o"
+  "StitchImages.pdb"
+  "StitchImages"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/StitchImages.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
